b4714756ebe0702a2d0049fc0014b3af18d8d3de
Silifonov Alexey <silifonov@gmail.com>
2019-09-03 13:57:17 +0300

    adding slick and server lists

---
+++ js/slick-slider-init.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // main slider
+    $('.main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        fade: false,
+        prevArrow: false,
+        nextArrow: false
+    });
+});
